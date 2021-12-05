b129ad83e90fea281238a768952dd0693279c46f
daagudeloc <daagudeloc@gmail.com>
2021-12-05 13:19:16 -0500

    Finished components practice up to video 9.

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {Course} from '../model/course'
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+
+  @Input()
+  course: Course;
+
+  @Output('courseSelected')
+  courseEmitter = new EventEmitter<Course>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onCourseView() {
+    console.log("Card component - button clicked...");
+
+    this.courseEmitter.emit(this.course);
+  }
+
+}
